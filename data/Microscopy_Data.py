15a25663d9d5fb96622c089f7f770108b1978ab2
Supawit <supawitch22@gmail.com>
2019-08-27 20:36:31 -0500

    Switch flip to mirror and rotate

--- data/Microscopy_Data.py
+++ data/Microscopy_Data.py
@@ -18,7 +18,7 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision.transforms.functional import vflip, hflip, rotate
 from torchvision import datasets, models, transforms
 
-from util import simulation
+# from util import simulation
 
 object_categories = ['T4', 'T4R', 'S1']
 
@@ -28,184 +28,200 @@ object_categories = ['T4', 'T4R', 'S1']
 
 
 def load_pil(img, shape=None):
-	img = Image.open(img)
-	if shape:
-		img = img.resize((shape, shape), Image.BILINEAR)
-	return np.array(img)
+    img = Image.open(img)
+    if shape:
+        img = img.resize((shape, shape), Image.BILINEAR)
+    return np.array(img)
 
 
 def generate_mask(mask_dir, img_name, shape=512, num_classes=8):
-	"""
+    """
 
-	:param num_classes: num of classes
-	:param mask_dir: root_dir/256_dataset/256/T4R/NA_T4R_122117_19/NA_T4R_122117_19_label
-	:param img_name: 11_5.png
-	:param shape: 256
-	:return: np.array -> (num_class, shape, shape)
-	"""
-	# dataset = osp.join(dataset, 'Mask')
-	# all_slides = os.listdir('/'.join(dataset.split('/')[:-1]))
-	# label_dir = f'{dataset}_label'
-	# raw_slide_name = dataset.split('/')[-1]
-	label_name = img_name + '.npy'
-	# print(label_name)
-	mask: np.ndarray = np.load(os.path.join(mask_dir, label_name))
-	# print(f'load {mask_dir}_{label_name}')
-	if mask.shape[0] != shape:
-		mask = cv2.resize(mask, (shape, shape), cv2.INTER_NEAREST)
-	# print(mask.shape)
-	assert np.ndim(mask) == 2 or np.ndim(mask) == 3
-	if np.ndim(mask) == 2:
-		one_hot_mask = np.ndarray((num_classes, *mask.shape))
-		one_hot_mask.fill(0)
-		for i in range(mask.shape[0]):
-			for j in range(mask.shape[1]):
-				one_hot_mask[mask[i][j]][i][j] = 1
-		target = one_hot_mask
-	else:
-		target = mask
+    :param num_classes: num of classes
+    :param mask_dir: root_dir/256_dataset/256/T4R/NA_T4R_122117_19/NA_T4R_122117_19_label
+    :param img_name: 11_5.png
+    :param shape: 256
+    :return: np.array -> (num_class, shape, shape)
+    """
+    # dataset = osp.join(dataset, 'Mask')
+    # all_slides = os.listdir('/'.join(dataset.split('/')[:-1]))
+    # label_dir = f'{dataset}_label'
+    # raw_slide_name = dataset.split('/')[-1]
+    label_name = img_name + '.npy'
+    # print(label_name)
+    mask: np.ndarray = np.load(os.path.join(mask_dir, label_name))
+    # print(f'load {mask_dir}_{label_name}')
+    if mask.shape[0] != shape:
+        mask = cv2.resize(mask, (shape, shape), cv2.INTER_NEAREST)
+    # print(mask.shape)
+    assert np.ndim(mask) == 2 or np.ndim(mask) == 3
+    if np.ndim(mask) == 2:
+        one_hot_mask = np.ndarray((num_classes, *mask.shape))
+        one_hot_mask.fill(0)
+        for i in range(mask.shape[0]):
+            for j in range(mask.shape[1]):
+                one_hot_mask[mask[i][j]][i][j] = 1
+        target = one_hot_mask
+    else:
+        target = mask
 
-	# if single_channel_target:
-	# 	masks = np.zeros((len(category) + 1, *shapes)).astype(np.int_)  # long
-	# 	# print(f'masks shape: {masks.shape}')
-	# 	for i in range(len(category)):
-	# 		if f'{raw_slide_name}_{category[i]}' in all_slides:
-	# 			mask = load_pil(osp.join(f'{dataset}_{category[i]}', img_name), shape=shape)
-	# 			masks[i + 1] = mask
-	# 	target_mask = np.argmax(masks, 0)  # bg->0
-	# elif include_bg:
-	# 	masks = np.zeros((len(category), *shapes)).astype(np.float_)  # long
-	# 	# print(f'masks shape: {masks.shape}')
-	# 	for i in range(len(category)):
-	# 		if f'{raw_slide_name}_{category[i]}' in all_slides:
-	# 			mask = load_pil(osp.join(f'{dataset}_{category[i]}', img_name), shape=shape)
-	# 			masks[i] = mask
-	# 	bg_mask = np.expand_dims(1 - np.clip(np.sum(masks, 0), 0, 1), axis=0)  # 1 * H * W. some pixels have
-	# 	# multi-label, visualization is needed
-	# 	target_mask = np.concatenate((bg_mask, masks), axis=0)  # 8*H*W, zero dimension as BG
-	# else:
-	# 	masks = np.zeros((len(category), *shapes)).astype(np.float_)  # long
-	# 	# print(f'masks shape: {masks.shape}')
-	# 	for i in range(len(category)):
-	# 		if f'{raw_slide_name}_{category[i]}' in all_slides:
-	# 			mask = load_pil(osp.join(f'{dataset}_{category[i]}', img_name), shape=shape)
-	# 			masks[i] = mask
-	# 	target_mask = masks
-	# #     print(f'mask after gen: {masks.shape}')
-	return target.astype(np.float32)
+    # if single_channel_target:
+    #   masks = np.zeros((len(category) + 1, *shapes)).astype(np.int_)  # long
+    #   # print(f'masks shape: {masks.shape}')
+    #   for i in range(len(category)):
+    #       if f'{raw_slide_name}_{category[i]}' in all_slides:
+    #           mask = load_pil(osp.join(f'{dataset}_{category[i]}', img_name), shape=shape)
+    #           masks[i + 1] = mask
+    #   target_mask = np.argmax(masks, 0)  # bg->0
+    # elif include_bg:
+    #   masks = np.zeros((len(category), *shapes)).astype(np.float_)  # long
+    #   # print(f'masks shape: {masks.shape}')
+    #   for i in range(len(category)):
+    #       if f'{raw_slide_name}_{category[i]}' in all_slides:
+    #           mask = load_pil(osp.join(f'{dataset}_{category[i]}', img_name), shape=shape)
+    #           masks[i] = mask
+    #   bg_mask = np.expand_dims(1 - np.clip(np.sum(masks, 0), 0, 1), axis=0)  # 1 * H * W. some pixels have
+    #   # multi-label, visualization is needed
+    #   target_mask = np.concatenate((bg_mask, masks), axis=0)  # 8*H*W, zero dimension as BG
+    # else:
+    #   masks = np.zeros((len(category), *shapes)).astype(np.float_)  # long
+    #   # print(f'masks shape: {masks.shape}')
+    #   for i in range(len(category)):
+    #       if f'{raw_slide_name}_{category[i]}' in all_slides:
+    #           mask = load_pil(osp.join(f'{dataset}_{category[i]}', img_name), shape=shape)
+    #           masks[i] = mask
+    #   target_mask = masks
+    # #     print(f'mask after gen: {masks.shape}')
+    return target.astype(np.float32)
 
 
 def read_object_labels(file, header=True, shuffle=True):
-	images = []
-	# num_categories = 0
-	print('[dataset] read', file)
-	with open(file, 'r') as f:
-		for line in f:
-			line_split = line.split(';')
-			if line_split.__len__() == 3:
-				img, segment_label, cell_type = line_split
-				cell_label = object_categories.index(cell_type.strip('\n'))
-				images.append((img, segment_label, cell_label))
-			else:
-				img, cell_type = line_split
-				cell_label = object_categories.index(cell_type.strip('\n'))
-				images.append((img, cell_label))
-	if shuffle:
-		random.shuffle(images)
-	return images
+    images = []
+    # num_categories = 0
+    print('[dataset] read', file)
+    with open(file, 'r') as f:
+        for line in f:
+            line_split = line.split(';')
+            if line_split.__len__() == 3:
+                img, segment_label, cell_type = line_split
+                cell_label = object_categories.index(cell_type.strip('\n'))
+                images.append((img, segment_label, cell_label))
+            elif line_split.__len__() == 2:
+                img, cell_type = line_split
+                cell_label = object_categories.index(cell_type.strip('\n'))
+                images.append((img, cell_label))
+    if shuffle:
+        random.shuffle(images)
+    return images
 
 
 class MicroscopyDataset(Dataset):
-	def __init__(self, root, train_list, img_size, output_size, transform=None, target_transform=None, crop_size=-1,
-				 h_flip=False,
-				 v_flip=False, single_channel_target=False, include_bg=True):
-		self.transform = transform
-		self.root = root
-		self.h_flip = h_flip
-		self.v_flip = v_flip
-		self.img_size = img_size
-		self.output_size = output_size
-		# self.classes = category
-		self.transform = transform
-		self.crop_size = crop_size
-		self.target_transform = target_transform
-		self.images = read_object_labels(train_list)
-		self.single_channel_target = single_channel_target
+    def __init__(
+            self,
+            root,
+            train_list,
+            img_size,
+            output_size,
+            transform=None,
+            target_transform=None,
+            crop_size=-1,
+            h_flip=False,
+            v_flip=False,
+            single_channel_target=False,
+            include_bg=True):
+        self.transform = transform
+        self.root = root
+        self.is_flip = h_flip or v_flip  # TODO: fix args
+        self.img_size = img_size
+        self.output_size = output_size
+        self.transform = transform
+        self.crop_size = crop_size
+        self.target_transform = target_transform
+        self.images = read_object_labels(train_list)
+        self.single_channel_target = single_channel_target
 
-	def __len__(self):
-		return len(self.images)
+    def __len__(self):
+        return len(self.images)
 
-	def get_number_classes(self):
-		return len(self.classes)
+    def get_number_classes(self):
+        return len(self.classes)
 
-	def __getitem__(self, index):
-		path, segment_label_path, cell_type = self.images[index]
-		if 'tif' in path:
-			img = tiff.imread(os.path.join(self.root, path))
-		else:
-			img = np.load(os.path.join(self.root, path))
-		mask = np.load(osp.join(self.root, segment_label_path)).copy()  # 2d mask
-		# print(mask.shape)
-		# img = Image.open(os.path.join(self.root, path)).convert('RGB')  # gray scale
-		if img.shape[0] != self.img_size:
-			img = cv2.resize(img, (self.img_size, self.img_size), cv2.INTER_NEAREST)
-		if mask.shape[0] != self.img_size:
-			# pdb.set_trace()
-			mask = cv2.resize(mask, (self.img_size, self.img_size), cv2.INTER_NEAREST)
-		# pdb.set_trace()
-		img = Image.fromarray(img)
-		# path_split = path.split('/')
-		# mask_dataset = '/'.join(path_split[:-1])
-		# mask_dataset = os.path.join(self.root, mask_dataset + '_label')
-		# img_name = path_split[-1]
-		if self.v_flip:
-			if np.random.random() < 0.5:
-				img = vflip(img)
-				mask = np.flip(mask, 0).copy()
-		if self.h_flip:
-			if np.random.random() < 0.5:
-				img = hflip(img)
-				mask = np.flip(mask, 1).copy()
+    def __getitem__(self, index):
+        # get metadata
+        path, segment_label_path, cell_type = self.images[index]
 
-		if self.transform is not None:
-			img = self.transform(img)
-		return [img, mask]
+        # load image
+        if 'tif' in osp.splitext(path)[-1].lower():
+            # load tiff image
+            img = tiff.imread(os.path.join(self.root, path))
+        else:
+            # load generic image
+            img = np.load(os.path.join(self.root, path))
+
+        # load mask
+        mask = np.load(osp.join(self.root, segment_label_path))
+
+        # scale image to fixed size
+        if img.shape[0] != self.img_size:
+            img = cv2.resize(
+                img, (self.img_size, self.img_size), cv2.INTER_NEAREST)
+        if mask.shape[0] != self.img_size:
+            mask = cv2.resize(
+                mask, (self.img_size, self.img_size), cv2.INTER_NEAREST)
+
+        # flipping: rotation + mirror (8 possibilities)
+        if self.is_flip:
+            if np.random.random() < 0.5:
+                # mirror
+                img = np.flip(img, 0)
+                mask = np.flip(mask, 0)
+            for _ in range(np.random.randint(4)):
+                # rotate 90 degree
+                img = np.rot90(img)
+                mask = np.rot90(mask)
+
+        # apply custom transform
+        img = Image.fromarray(img)
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, mask
 
 
 class TiffDataset(Dataset):
-	def __init__(self, root, train_list, img_size, output_size, transform=None, target_transform=None, crop_size=-1,
-				 h_flip=False,
-				 v_flip=False, single_channel_target=False, include_bg=True):
-		self.transform = transform
-		self.root = root
-		self.h_flip = h_flip
-		self.v_flip = v_flip
-		self.img_size = img_size
-		self.output_size = output_size
-		# self.classes = category
-		self.transform = transform
-		self.crop_size = crop_size
-		self.target_transform = target_transform
-		self.images = read_object_labels(train_list)
-		self.single_channel_target = single_channel_target
+    def __init__(self, root, train_list, img_size, output_size, transform=None, target_transform=None, crop_size=-1,
+                 h_flip=False,
+                 v_flip=False, single_channel_target=False, include_bg=True):
+        self.transform = transform
+        self.root = root
+        self.h_flip = h_flip
+        self.v_flip = v_flip
+        self.img_size = img_size
+        self.output_size = output_size
+        # self.classes = category
+        self.transform = transform
+        self.crop_size = crop_size
+        self.target_transform = target_transform
+        self.images = read_object_labels(train_list)
+        self.single_channel_target = single_channel_target
 
-	def __len__(self):
-		return len(self.images)
+    def __len__(self):
+        return len(self.images)
 
-	def get_number_classes(self):
-		return len(self.classes)
+    def get_number_classes(self):
+        return len(self.classes)
 
-	def __getitem__(self, index):
-		path, segment_label_path, cell_type = self.images[index]
-		img = np.load(os.path.join(self.root, path))
-		# img = Image.open(os.path.join(self.root, path)).convert('RGB')  # gray scale
-		if img.shape[0] != self.img_size:
-			img = cv2.resize(img, (self.img_size, self.img_size), cv2.INTER_NEAREST)
-		img = Image.fromarray(img)
+    def __getitem__(self, index):
+        path, segment_label_path, cell_type = self.images[index]
+        img = np.load(os.path.join(self.root, path))
+        # img = Image.open(os.path.join(self.root, path)).convert('RGB')  # gray scale
+        if img.shape[0] != self.img_size:
+            img = cv2.resize(img, (self.img_size, self.img_size), cv2.INTER_NEAREST)
+        img = Image.fromarray(img)
 
-		if self.transform is not None:
-			img = self.transform(img)
-		return [img]
+        if self.transform is not None:
+            img = self.transform(img)
+        return [img]
 
 
 # def __getitem__(self, idx):
@@ -240,10 +256,10 @@ class TiffDataset(Dataset):
 #         img = Image.open(os.path.join(self.root, path)).convert('RGB')
 #         img = img.resize((self.img_size, self.img_size), Image.BILINEAR)
 #         # if self.crop_size > 0:
-#         # 	start_w = int((self.img_size - self.crop_size) * np.random.random())
-#         # 	start_h = int((self.img_size - self.crop_size) * np.random.random())
-#         # 	img = img.crop((start_w, start_h, start_w +
-#         # 					self.crop_size, start_h + self.crop_size))
+#         #     start_w = int((self.img_size - self.crop_size) * np.random.random())
+#         #     start_h = int((self.img_size - self.crop_size) * np.random.random())
+#         #     img = img.crop((start_w, start_h, start_w +
+#         #                     self.crop_size, start_h + self.crop_size))
 
 #         if self.transform is not None:
 #             img = self.transform(img)
@@ -259,11 +275,11 @@ class TiffDataset(Dataset):
 
 
 def reverse_transform(inp):
-	inp = inp.numpy().transpose((1, 2, 0))
-	mean = np.array([0.485, 0.456, 0.406])
-	std = np.array([0.229, 0.224, 0.225])
-	inp = std * inp + mean
-	inp = np.clip(inp, 0, 1)
-	inp = (inp * 255).astype(np.uint8)
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    inp = (inp * 255).astype(np.uint8)
 
-	return inp
+    return inp
