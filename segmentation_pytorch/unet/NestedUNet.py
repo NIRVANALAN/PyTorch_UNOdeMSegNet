97e6f7c80ee4071606b3dc76047451e07edeb5e3
NIRVANALAN <maverickers@outlook.com>
2020-03-20 14:16:05 +0000

    update multiresunet

--- segmentation_pytorch/unet/NestedUNet.py
+++ segmentation_pytorch/unet/NestedUNet.py
@@ -41,38 +41,54 @@ class UNet(nn.Module):
         self.up = nn.Upsample(
             scale_factor=2, mode='bilinear', align_corners=True)
 
+        self.path_1 = VGGBlock(dim_in, nb_filter[0], nb_filter[0])
+        self.path_2 = VGGBlock(dim_in, nb_filter[1], nb_filter[1])
+        self.path_3 = VGGBlock(dim_in, nb_filter[2], nb_filter[2])
+        self.path_4 = VGGBlock(dim_in, nb_filter[3], nb_filter[3])
+
         self.conv0_0 = VGGBlock(dim_in,
                                 nb_filter[0], nb_filter[0])
-        self.conv1_0 = VGGBlock(nb_filter[0], nb_filter[1], nb_filter[1])
-        self.conv2_0 = VGGBlock(nb_filter[1], nb_filter[2], nb_filter[2])
-        self.conv3_0 = VGGBlock(nb_filter[2], nb_filter[3], nb_filter[3])
-        self.conv4_0 = VGGBlock(nb_filter[3], nb_filter[4], nb_filter[4])
+        self.conv1_0 = VGGBlock(nb_filter[0]*2, nb_filter[1], nb_filter[1])
+        self.conv2_0 = VGGBlock(nb_filter[1]*2, nb_filter[2], nb_filter[2])
+        self.conv3_0 = VGGBlock(nb_filter[2]*2, nb_filter[3], nb_filter[3])
+        self.conv4_0 = VGGBlock(nb_filter[3]*2, nb_filter[4], nb_filter[4])
 
         self.conv3_1 = VGGBlock(
-            nb_filter[3]+nb_filter[4], nb_filter[3], nb_filter[3])
+            nb_filter[3]*2+nb_filter[4], nb_filter[3], nb_filter[3])
         self.conv2_2 = VGGBlock(
-            nb_filter[2]+nb_filter[3], nb_filter[2], nb_filter[2])
+            nb_filter[2]*2+nb_filter[3], nb_filter[2], nb_filter[2])
         self.conv1_3 = VGGBlock(
-            nb_filter[1]+nb_filter[2], nb_filter[1], nb_filter[1])
+            nb_filter[1]*2+nb_filter[2], nb_filter[1], nb_filter[1])
         self.conv0_4 = VGGBlock(
-            nb_filter[0]+nb_filter[1], nb_filter[0], nb_filter[0])
+            nb_filter[0]*2+nb_filter[1], nb_filter[0], nb_filter[0])
 
-        self.final = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
+        self.final1 = nn.Conv2d(nb_filter[3], n_classes, kernel_size=1)
+        self.final2 = nn.Conv2d(nb_filter[2], n_classes, kernel_size=1)
+        self.final3 = nn.Conv2d(nb_filter[1], n_classes, kernel_size=1)
+        self.final4 = nn.Conv2d(nb_filter[0], n_classes, kernel_size=1)
 
     def forward(self, input):
-        x0_0 = self.conv0_0(input)
-        x1_0 = self.conv1_0(self.pool(x0_0))
-        x2_0 = self.conv2_0(self.pool(x1_0))
-        x3_0 = self.conv3_0(self.pool(x2_0))
-        x4_0 = self.conv4_0(self.pool(x3_0))
+        assert len(input) = 5
+        x0_0 = self.conv0_0(input[0])
+        x1_0 = self.conv1_0(
+            torch.cat([self.pool(x0_0), self.path_1(input[1])]))
+        x2_0 = self.conv2_0(
+            torch.cat([self.pool(x1_0), self.path_2(input[2])]))
+        x3_0 = self.conv3_0(
+            torch.cat([self.pool(x2_0), self.path_3(input[3])]))
+        x4_0 = self.conv4_0(
+            torch.cat([self.pool(x3_0), self.path_4(input[4])]))
 
         x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
         x2_2 = self.conv2_2(torch.cat([x2_0, self.up(x3_1)], 1))
         x1_3 = self.conv1_3(torch.cat([x1_0, self.up(x2_2)], 1))
         x0_4 = self.conv0_4(torch.cat([x0_0, self.up(x1_3)], 1))
 
-        output = self.final(x0_4)
-        return output
+        output1 = self.final1(x0_1)
+        output2 = self.final2(x0_2)
+        output3 = self.final3(x0_3)
+        output4 = self.final4(x0_4)
+        return [output1, output2, output3, output4]
 
 
 class MultiResUNet(nn.Module):
